ef96da2115790830208f17b54f6d67f28fe1124c
JSON <JSON@CodeChomper.com>
2017-07-31 10:32:41 -0700

    Zombie Spawn Fixed Level2 50%

--- scripts/ZombieSpawn.gd
+++ scripts/ZombieSpawn.gd
@@ -12,10 +12,11 @@ func _ready():
 
 func _on_SpawnTimer_timeout():
 	#count how many badguys
+	spawnTimer.set_wait_time(rand_range(8,20))
+	spawnTimer.start()
+	print(str(get_tree().get_nodes_in_group("badGuys").size()))
 	if get_tree().get_nodes_in_group("badGuys").size() < 4:
 		print('adding zombie now')
-		spawnTimer.set_wait_time(rand_range(8,20))
-		spawnTimer.start()
 		var z = zombie.instance()
 		z.set_global_pos(get_global_pos())
 		get_parent().add_child(z)
